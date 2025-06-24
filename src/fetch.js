fe8763cc5a76f6f32dbf9812cc77e5b714252296
root <bolha.matheus@grupofleury.com.br>
2025-06-24 14:56:51 -0300

    feat: adjust to use cosmic api v3

--- src/fetch.js
+++ src/fetch.js
@@ -147,7 +147,7 @@ module.exports = async ({
 
   // Map and clean data.
   if (objects.length > 0) {
-    objects = objects.map(item => clean(item))
+    objects = objects.map((item) => clean(item))
   }
   return objects
 }
@@ -158,7 +158,7 @@ module.exports = async ({
  * @param {object} item - Entry needing clean
  * @returns {object} output - Object cleaned
  */
-const clean = item => {
+const clean = (item) => {
   _.forEach(item, (value, key) => {
     if (_.startsWith(key, `__`)) {
       delete item[key]
